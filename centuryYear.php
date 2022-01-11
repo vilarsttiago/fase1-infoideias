e141878a0c4b88bae8213a7a3613fa1c1622ac9c
Tiago Vilar <vilarsttiago@gmail.com>
2022-01-10 21:21:48 -0300

    Update centuryYear.php

--- centuryYear.php
+++ centuryYear.php
@@ -1,14 +1,27 @@
-/**
- * @author Tiago Vilar <vilarsttiago@gmail.com>
- * 
- * 
- * @param float $year
- * @return float
- * 
- * **/
+<?php
 
-function centuryYear($year){
-    $century = $year / 100;
-    
-    return ceil($century);
-}
+    /**
+     * @author Tiago Vilar <vilarsttiago@gmail.com>
+     * 
+     * 
+     * @param int $year
+     * @return float
+     * @return int
+     * 
+     * **/
+
+    function centuryYear($year){
+
+        if(is_int($year)){
+
+            $century = $year / 100;
+
+            return ceil($century); 
+
+        }else{
+            return -1;
+        }
+
+    }
+
+?>
